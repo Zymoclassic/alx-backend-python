52d46093aa26de0664c21b82d3309f4d12d592ac
Awopeju Kolawole Moses <Awopejumoses@gmail.com>
2024-01-12 01:42:25 +0100

    task 2, 3 and 4

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+A type-annonated function.
+'''
+
+
+def to_str(n: float) -> str:
+
+    '''
+    Converts a floating-point number to a string.
+    '''
+    return str(n)
