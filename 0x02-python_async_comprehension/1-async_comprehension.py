1c0db219ecc5d800dcb8c9a9a92518ce4ccc1a6e
Awopeju Kolawole Moses <Awopejumoses@gmail.com>
2024-01-16 22:16:22 +0100

    sorted all

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""
+async comprehension
+"""
+import asyncio
+import random
+from typing import Generator
+from typing import List
+from importlib import import_module as theModule
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    create random list of 10 numbers
+    """
+    return [x async for x in async_generator()]
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""
+measure runtime
+"""
+import asyncio
+import time
+from importlib import import_module as theModule
+
+async_comprehension = theModule('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Executes the function four times
+    """
+    startTime = time.time()
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    return time.time() - startTime
