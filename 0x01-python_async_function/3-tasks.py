59e36a52fcab5df252a2005ceb87b5e7c66277f8
Awopeju Kolawole Moses <Awopejumoses@gmail.com>
2024-01-16 09:03:56 +0100

    fixed

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -4,3 +4,10 @@ task_3
 """
 
 import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Executes task_3.
+    """
+    return asyncio.create_task(wait_random(max_delay))
